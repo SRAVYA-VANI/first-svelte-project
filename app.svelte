b6c45fa0bc3d35a2f36169d546cb808103ee9996
SRAVYA-VANI <70704736+SRAVYA-VANI@users.noreply.github.com>
2021-06-16 12:27:52 +0530

    Create app.svelte

---
+++ app.svelte
@@ -0,0 +1,33 @@
+<script>
+    let firstname ="";
+	let src ="favicon.png"
+    let lastname = "";
+	
+	
+	</script>
+
+<main>
+    <h1>hello!this is {firstname}!!!!</h1>
+    <label for="firstname">FIRST NAME</label>
+	<input type="text" bind:value={firstname}>
+	<label for="lastname">LAST NAME</label>
+    <input type="text" bind:value={lastname}>
+
+
+	<img src ={src} alt="svelte logo">
+
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+        align-items: center;
+	}
+    h1{
+        font-style: italic;
+        font-weight: bold;
+        font-size: xx-large;
+        text-align: center;
+	}
+</style>
